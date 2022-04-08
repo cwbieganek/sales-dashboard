26c5f2f3d0797247dc1d6e328ee59a41fd0356a9
Chris Bieganek <cwbieganek@gmail.com>
2022-04-08 18:31:59 -0500

    Add tailwindcss classes to <html> and <body> tags

--- pages/_document.js
+++ pages/_document.js
@@ -2,11 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
 	return (
-		<Html>
+		<Html className="h-full bg-gray-100">
 			<Head>
 				<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
 			</Head>
-			<body>
+			<body className="h-full">
 				<Main />
 				<NextScript />
 			</body>

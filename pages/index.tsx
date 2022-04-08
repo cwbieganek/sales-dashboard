53f5ac874350b11ea08f020dce76a520505905ba
Chris Bieganek <cwbieganek@gmail.com>
2022-04-08 18:46:41 -0500

    Add Stats component

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,12 @@ const userNavigation = [
   { name: 'Sign out', href: '#' },
 ]
 
+const stats = [
+  { name: 'Total Subscribers', stat: '71,897' },
+  { name: 'Avg. Open Rate', stat: '58.16%' },
+  { name: 'Avg. Click Rate', stat: '24.57%' },
+]
+
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
@@ -237,11 +243,19 @@ export default function Example() {
                 <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
               </div>
               <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-                {/* Replace with your content */}
-                <div className="py-4">
-                  <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+                {/* Stats component */}
+                <div>
+                  <h3 className="text-lg leading-6 font-medium text-gray-900">Last 30 days</h3>
+                  <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+                    {stats.map((item) => (
+                      <div key={item.name} className="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
+                        <dt className="text-sm font-medium text-gray-500 truncate">{item.name}</dt>
+                        <dd className="mt-1 text-3xl font-semibold text-gray-900">{item.stat}</dd>
+                      </div>
+                    ))}
+                  </dl>
                 </div>
-                {/* /End replace */}
+                {/* end Stats component */}
               </div>
             </div>
           </main>

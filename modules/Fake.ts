e21c0ddbf657d4770063d0027d676a0d080f9c52
Chris Bieganek <cwbieganek@gmail.com>
2022-04-09 18:50:14 -0500

    Don't provide any percentage attributes for Review stat

--- modules/Fake.ts
+++ modules/Fake.ts
@@ -4,6 +4,6 @@ export const stats: Statistic[] = [
   { name: 'Total Sales', stat: '10,142', percentageChange: 10.2, percentageImpact: "GOOD"},
   { name: 'Gross Revenue', stat: '$202,840', percentageChange: 14.9, percentageImpact: "GOOD" },
   { name: 'Net Profit', stat: '$50,710', percentageChange: 11.5, percentageImpact: "GOOD" },
-  { name: 'Reviews', stat: '127', percentageChange: 5.0, percentageImpact: "GOOD" },
+  { name: 'Reviews', stat: '127' },
   { name: 'Returns', stat: '250', percentageChange: 21.2, percentageImpact: "BAD" },
 ];

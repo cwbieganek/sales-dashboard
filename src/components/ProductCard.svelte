4a4ada99cb6c171f9162f6ea1035e0671915e24a
Chris Bieganek <cwbieganek@gmail.com>
2023-10-27 08:59:34 -0500

    Remove product tags

--- src/components/ProductCard.svelte
+++ src/components/ProductCard.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="max-w-sm rounded overflow-hidden shadow-lg">
-	<img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains" />
+	<img class="w-full" src="/img/card-top.jpg" alt={product.description} />
 	<div class="px-6 py-4">
 		<div class="font-bold text-xl mb-2">{product.name}</div>
 		<div class="text-gray-700 text-base">
@@ -23,19 +23,5 @@
 				<div>{product.sales.toLocaleString()}</div>
 			</div>
 		</div>
-	</div>
-	<div class="px-6 pt-4 pb-2">
-		<span
-			class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-			>#photography</span
-		>
-		<span
-			class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-			>#travel</span
-		>
-		<span
-			class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-			>#winter</span
-		>
 	</div>
 </div>

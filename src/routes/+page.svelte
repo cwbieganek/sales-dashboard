10e2e091627783169dc6c1ab7625d958d01cf4a0
Chris Bieganek <cwbieganek@gmail.com>
2023-10-27 08:46:39 -0500

    Create /products route and minimal homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,2 @@
-<script lang="ts">
-	import { ProductCategory, type Product } from '../components/Product';
-	import ProductCard from '../components/ProductCard.svelte';
-
-	const exampleProduct: Product = {
-		name: 'Coffee Maker',
-		description: 'A fancy coffee maker that brews the best coffee possible.',
-		listPrice: 40.0,
-		wholesaleCost: 20.0,
-		sales: 1000,
-		category: ProductCategory.Kitchen,
-	};
-</script>
-
-<h1 class="text-xl text-center font-semibold border-b border-gray-400 p-4">Products</h1>
-<div class="grid grid-cols-4 gap-4 p-4">
-	<ProductCard product={exampleProduct} />
-	<ProductCard product={exampleProduct} />
-	<ProductCard product={exampleProduct} />
-	<ProductCard product={exampleProduct} />
-	<ProductCard product={exampleProduct} />
-	<ProductCard product={exampleProduct} />
-	<ProductCard product={exampleProduct} />
-</div>
+<h1>Welcome to your Sales Dashboard!</h1>
+<a href="/products">View your products</a>
---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { ProductCategory, type Product } from '../../components/Product';
+	import ProductCard from '../../components/ProductCard.svelte';
+
+	const exampleProduct: Product = {
+		name: 'Coffee Maker',
+		description: 'A fancy coffee maker that brews the best coffee possible.',
+		listPrice: 40.0,
+		wholesaleCost: 20.0,
+		sales: 1000,
+		category: ProductCategory.Kitchen,
+	};
+</script>
+
+<h1 class="text-xl text-center font-semibold border-b border-gray-400 p-4">Products</h1>
+<div class="grid grid-cols-4 gap-4 p-4">
+	<ProductCard product={exampleProduct} />
+	<ProductCard product={exampleProduct} />
+	<ProductCard product={exampleProduct} />
+	<ProductCard product={exampleProduct} />
+	<ProductCard product={exampleProduct} />
+	<ProductCard product={exampleProduct} />
+	<ProductCard product={exampleProduct} />
+</div>

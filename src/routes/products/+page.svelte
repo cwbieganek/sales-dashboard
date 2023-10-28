06f4804240f3186eeccdc4664bed55cf6d258615
Chris Bieganek <cwbieganek@gmail.com>
2023-10-28 14:10:30 -0500

    Add Sort By and Sort Order dropdowns

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -48,6 +48,33 @@
 				bind:value={nameQuery}
 			/>
 		</div>
+		<!-- Sort By -->
+		<div>
+			<label for="sort-by" class="block text-sm font-medium leading-6 text-gray-900">Sort By</label>
+			<select
+				id="sort-by"
+				name="sort-by"
+				class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+			>
+				<option selected>List Price</option>
+				<option>Wholesale Cost</option>
+				<option>Sales</option>
+			</select>
+		</div>
+		<!-- Sort Order -->
+		<div>
+			<label for="sort-order" class="block text-sm font-medium leading-6 text-gray-900"
+				>Sort Order</label
+			>
+			<select
+				id="sort-order"
+				name="sort-order"
+				class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+			>
+				<option selected>Ascending</option>
+				<option>Descending</option>
+			</select>
+		</div>
 	</div>
 </div>
 <div class="grid grid-cols-4 gap-2 p-2">

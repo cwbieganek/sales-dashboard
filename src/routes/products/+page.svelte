ac883aed344e59c5f8555fb4a753f6eadc33a708
Chris Bieganek <cwbieganek@gmail.com>
2023-10-28 11:16:59 -0500

    Update the search input placeholder

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -43,7 +43,7 @@
 				id="search"
 				name="search"
 				class="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6"
-				placeholder="Search products"
+				placeholder="Search products by name"
 				type="search"
 				bind:value={nameQuery}
 			/>

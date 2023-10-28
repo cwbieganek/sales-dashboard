66154717c48a49a95bc9acfed9375c9f358f2484
Chris Bieganek <cwbieganek@gmail.com>
2023-10-28 10:36:34 -0500

    Use zod to parse the imported JSON products. Render the imported products in an each loop.

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,16 +1,11 @@
 <script lang="ts">
-	import { ProductCategory, type Product } from '../../components/Product';
+	import { z } from 'zod';
+	import type { Product } from '../../components/Product';
+	import { productsSchema } from './productsSchema';
 	import ProductCard from '../../components/ProductCard.svelte';
+	import rawProducts from './products.json';
 
-	const exampleProduct: Product = {
-		name: 'Coffee Maker',
-		description: 'A fancy coffee maker that brews the best coffee possible.',
-		listPrice: 40.0,
-		wholesaleCost: 20.0,
-		sales: 1000,
-		category: ProductCategory.Kitchen,
-		thumbnail: '/img/card-top.jpg',
-	};
+	const products = productsSchema.parse(rawProducts) as Product[];
 </script>
 
 <h1 class="text-xl text-center font-semibold border-b border-gray-400 p-4 mt-4 mb-4">Products</h1>
@@ -44,11 +39,7 @@
 	</div>
 </div>
 <div class="grid grid-cols-4 gap-4 p-4">
-	<ProductCard product={exampleProduct} />
-	<ProductCard product={exampleProduct} />
-	<ProductCard product={exampleProduct} />
-	<ProductCard product={exampleProduct} />
-	<ProductCard product={exampleProduct} />
-	<ProductCard product={exampleProduct} />
-	<ProductCard product={exampleProduct} />
+	{#each products as product}
+		<ProductCard {product} />
+	{/each}
 </div>

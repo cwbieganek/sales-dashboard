2ce6ffc3a71902c27685913e65f020a1cea4cea5
Chris <cwbieganek@gmail.com>
2023-10-29 10:44:10 -0500

    Allow sorting of products using any Product property (#6)
    * Add Sort By and Sort Order dropdowns
    
    * Set the focus border of the dropdowns to black
    
    * Default to sorting products by ascending list price
    
    * Document searchProductsByName
    
    * Set the label + dropdown containers to flex row. Add right margin to the labels.
    
    * Move Sort By and Sort Order dropdowns out of the Search Bar container
    
    * Update the position and styling of the inputs
    
    * Add sortKey parameter to searchProductsByName. Start by sorting the products by name.
    
    * Bind the Sort By dropdown value to sortKey
    
    * Add sortOrder parameter to searchProductsByName and bind it to the dropdown value

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -4,25 +4,60 @@
 	import ProductCard from '../../components/ProductCard.svelte';
 	import rawProducts from './products.json';
 
+	enum SortOrder {
+		ASCENDING = 'Ascending',
+		DESCENDING = 'Descending',
+	}
+
 	const productsFromJson = productsSchema.parse(rawProducts) as Product[];
 	let nameQuery = '';
-	$: filteredProducts = searchProductsByName(productsFromJson, nameQuery);
+	let sortKey: keyof Product = 'name';
+	let sortOrder = SortOrder.ASCENDING;
+	$: filteredProducts = searchProductsByName(productsFromJson, nameQuery, sortKey, sortOrder);
 
-	function searchProductsByName(products: Product[], nameQuery: string): Product[] {
+	/**
+	 * Searches the list of products for all products whose name contains the name query.
+	 * The returned list will be sorted by the provided Product key.
+	 */
+	function searchProductsByName<K extends keyof Product>(
+		products: Product[],
+		nameQuery: string,
+		sortKey: K,
+		sortOrder = SortOrder.ASCENDING
+	): Product[] {
 		if (nameQuery.trim() == '') {
-			return products;
+			return products.toSorted((a, b) => {
+				if (a[sortKey] < b[sortKey]) {
+					return sortOrder === SortOrder.ASCENDING ? -1 : 1;
+				} else if (a[sortKey] > b[sortKey]) {
+					return sortOrder === SortOrder.ASCENDING ? 1 : -1;
+				}
+
+				return 0;
+			});
 		}
 
-		return products.filter((product) => {
-			return product.name.toLowerCase().includes(nameQuery.toLowerCase());
-		});
+		return products
+			.filter((product) => {
+				return product.name.toLowerCase().includes(nameQuery.toLowerCase());
+			})
+			.toSorted((a, b) => {
+				if (a[sortKey] < b[sortKey]) {
+					return sortOrder === SortOrder.ASCENDING ? -1 : 1;
+				} else if (a[sortKey] > b[sortKey]) {
+					return sortOrder === SortOrder.ASCENDING ? 1 : -1;
+				}
+
+				return 0;
+			});
 	}
 </script>
 
 <h1 class="text-xl text-center font-semibold border-b border-gray-400 p-4 mt-4 mb-4">Products</h1>
-<!-- Product Search -->
-<div class="flex items-center justify-center">
+<!-- Product Search and Sorting-->
+<div class="flex items-center justify-center gap-2">
 	<div class="w-full max-w-lg lg:max-w-xs">
+		<!-- Search Bar -->
 		<label for="search" class="sr-only">Search</label>
 		<div class="relative">
 			<div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
@@ -49,6 +84,36 @@
 			/>
 		</div>
 	</div>
+	<!-- Sort By -->
+	<label for="sort-by" class="block text-sm font-medium leading-6 text-gray-900 whitespace-nowrap"
+		>Sort By</label
+	>
+	<select
+		id="sort-by"
+		name="sort-by"
+		class="block rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-black sm:text-sm sm:leading-6"
+		bind:value={sortKey}
+	>
+		<!-- All these option values are keys of the Product type -->
+		<option value="name" selected>Name</option>
+		<option value="listPrice">List Price</option>
+		<option value="wholesaleCost">Wholesale Cost</option>
+		<option value="sales">Sales</option>
+	</select>
+	<!-- Sort Order -->
+	<label
+		for="sort-order"
+		class="block text-sm font-medium leading-6 text-gray-900 whitespace-nowrap">Sort Order</label
+	>
+	<select
+		id="sort-order"
+		name="sort-order"
+		class="block rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-black sm:text-sm sm:leading-6"
+		bind:value={sortOrder}
+	>
+		<option value="Ascending" selected>Ascending</option>
+		<option value="Descending">Descending</option>
+	</select>
 </div>
 <div class="grid grid-cols-4 gap-2 p-2">
 	{#each filteredProducts as product}

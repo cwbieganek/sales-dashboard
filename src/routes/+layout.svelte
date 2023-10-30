0320363e2203eb41a1d6c88457abc203f8064608
Chris Bieganek <cwbieganek@gmail.com>
2023-10-29 20:58:41 -0500

    Add Tailwind UI dark theme navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
 	import '../app.css';
+	import Navbar from './Navbar.svelte';
 </script>
 
 <div class="app">
-	<main>
+	<main class="bg-gray-100">
+		<Navbar />
 		<slot />
 	</main>
 </div>

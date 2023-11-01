1b8ff00c71828aac8cdd382c61029f73e4ed224b
Chris Bieganek <cwbieganek@gmail.com>
2023-11-01 07:57:50 -0500

    Add the ProductSalesChart to the product statistics page

--- src/routes/products/[id]/+page.svelte
+++ src/routes/products/[id]/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+	import ProductSalesChart from '../../../components/ProductSalesChart.svelte';
+
 	export let data;
 </script>
 
 <div class="h-full">
 	<h1>{data.product.name} #{data.product.id}</h1>
+	<div class="w-6/12">
+		<ProductSalesChart />
+	</div>
 </div>

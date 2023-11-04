cd05928090247c0d0aabad6631b15909aed9f78b
Chris Bieganek <cwbieganek@gmail.com>
2023-11-03 19:16:00 -0500

    Add min-h-screen everywhere so that the content always fills the screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,8 @@
 	import Navbar from './Navbar.svelte';
 </script>
 
-<div class="app">
-	<main class="bg-gray-100">
+<div class="app min-h-screen">
+	<main class="bg-gray-100 min-h-screen">
 		<Navbar />
 		<slot />
 	</main>
--- src/routes/products/[id]/+page.svelte
+++ src/routes/products/[id]/+page.svelte
@@ -5,8 +5,8 @@
 </script>
 
 <div class="h-full">
-	<h1>{data.product.name} #{data.product.id}</h1>
-	<div class="w-6/12">
+	<h1 class="text-center">{data.product.name} (Product #{data.product.id})</h1>
+	<div class="w-6/12 m-auto h-full">
 		<ProductSalesChart />
 	</div>
 </div>

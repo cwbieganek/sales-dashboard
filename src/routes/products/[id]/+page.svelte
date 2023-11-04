eb68a2afcaa61c924d277f15ffc54a4c7e4e1992
Chris Bieganek <cwbieganek@gmail.com>
2023-11-04 11:53:43 -0500

    Redesign the product name header

--- src/routes/products/[id]/+page.svelte
+++ src/routes/products/[id]/+page.svelte
@@ -5,8 +5,9 @@
 </script>
 
 <div class="h-full">
-	<h1 class="text-center">{data.product.name} (Product #{data.product.id})</h1>
-	<div class="w-3/12 mx-auto mb-2">
+	<h1 class="flex items-center justify-center align-middle bg-gray-200 h-10">
+		{data.product.name} (Product #{data.product.id})
+	</h1>
 		<img src={data.product.thumbnail} alt={data.product.name} class="rounded-sm" />
 	</div>
 	<div class="w-6/12 m-auto h-full">

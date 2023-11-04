fce103db34dd7f3ec0fa476ca2e7f8a772faeb10
Chris Bieganek <cwbieganek@gmail.com>
2023-11-04 11:55:33 -0500

    Add mt-2 to the thumbnail container

--- src/routes/products/[id]/+page.svelte
+++ src/routes/products/[id]/+page.svelte
@@ -8,6 +8,7 @@
 	<h1 class="flex items-center justify-center align-middle bg-gray-200 h-10">
 		{data.product.name} (Product #{data.product.id})
 	</h1>
+	<div class="w-3/12 mx-auto mt-2">
 		<img src={data.product.thumbnail} alt={data.product.name} class="rounded-sm" />
 	</div>
 	<div class="w-6/12 m-auto h-full">

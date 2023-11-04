712c40f80e0c7fcd99471c3aaa78f7170331b752
Chris Bieganek <cwbieganek@gmail.com>
2023-11-04 11:31:17 -0500

    Add the product thumbnail

--- src/routes/products/[id]/+page.svelte
+++ src/routes/products/[id]/+page.svelte
@@ -6,6 +6,9 @@
 
 <div class="h-full">
 	<h1 class="text-center">{data.product.name} (Product #{data.product.id})</h1>
+	<div class="w-3/12 mx-auto mb-2">
+		<img src={data.product.thumbnail} alt={data.product.name} class="rounded-sm" />
+	</div>
 	<div class="w-6/12 m-auto h-full">
 		<ProductSalesChart salesByMonth={data.product.salesByMonth} />
 	</div>

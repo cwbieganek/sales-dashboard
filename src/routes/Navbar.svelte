7f769b8fbb67e452c28e52750c7ddda9e321d4d0
Chris Bieganek <cwbieganek@gmail.com>
2023-11-08 08:21:12 -0600

    Implement the Figma navbar design

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,93 +1,54 @@
-<nav class="bg-gray-800">
-	<div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-		<div class="relative flex h-16 items-center justify-between">
-			<div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-				<!-- Mobile menu button-->
-				<button
-					type="button"
-					class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-					aria-controls="mobile-menu"
-					aria-expanded="false"
-				>
-					<span class="absolute -inset-0.5" />
-					<span class="sr-only">Open main menu</span>
-					<!--
-            Icon when menu is closed.
-
-            Menu open: "hidden", Menu closed: "block"
-          -->
-					<svg
-						class="block h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						aria-hidden="true"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-						/>
-					</svg>
-					<!--
-            Icon when menu is open.
-
-            Menu open: "block", Menu closed: "hidden"
-          -->
-					<svg
-						class="hidden h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						aria-hidden="true"
-					>
-						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-					</svg>
-				</button>
-			</div>
-			<div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-				<div class="flex flex-shrink-0 items-center">
-					<img
-						class="h-8 w-auto"
-						src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-						alt="Your Company"
-					/>
-				</div>
-				<div class="hidden sm:ml-6 sm:block">
-					<div class="flex space-x-4">
-						<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-						<a
-							href="/products"
-							class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
-							aria-current="page">Products</a
-						>
-						<a
-							href="#"
-							class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-							>Financials</a
-						>
-					</div>
-				</div>
-			</div>
-		</div>
+<nav class="bg-gray-800 h-20 flex flex-row justify-evenly text-white px-4">
+	<div class="flex flex-row items-center gap-2 w-4/12 text-brand font-semibold text-2xl">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="w-10 h-10"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+			/>
+		</svg>
+		Sales Dashboard
 	</div>
-
-	<!-- Mobile menu, show/hide based on menu state. -->
-	<div class="sm:hidden" id="mobile-menu">
-		<div class="space-y-1 px-2 pb-3 pt-2">
-			<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-			<a
-				href="#"
-				class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
-				aria-current="page">Products</a
-			>
-			<a
-				href="#"
-				class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-				>Financials</a
-			>
-		</div>
+	<div class="flex flex-row justify-center items-center align-middle w-4/12 gap-6">
+		<a href="#" class="underline decoration-brand decoration-2 pointer-events-none" aria-current
+			>PRODUCTS</a
+		>
+		<a href="#" class="hover:underline">ORDERS</a>
+	</div>
+	<div class="flex flex-row flex-grow items-center justify-end gap-2">
+		<button
+			type="button"
+			class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+			id="user-menu-button"
+			aria-expanded="false"
+			aria-haspopup="true"
+		>
+			<span class="absolute -inset-1.5" />
+			<span class="sr-only">Open user menu</span>
+			<img
+				class="h-8 w-8 rounded-full"
+				src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+				alt=""
+			/>
+		</button>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			fill="currentColor"
+			class="w-10 h-10"
+		>
+			<path
+				fill-rule="evenodd"
+				d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+				clip-rule="evenodd"
+			/>
+		</svg>
 	</div>
 </nav>

c64bac49defdfa46bec40ff6baae530e161f1936
Chris Bieganek <cwbieganek@gmail.com>
2023-11-17 18:41:26 -0600

    Add Stat component

---
+++ src/components/Stat.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { GoodOrBad } from './stat';
+
+	export let title: string;
+	export let num: number;
+	export let goodOrBad: GoodOrBad;
+	export let change: number;
+	export let formatAsCurrency = false;
+
+	const changeTextColor = goodOrBad === GoodOrBad.GOOD ? 'text-lime-500' : 'text-red-500';
+</script>
+
+<div class="flex flex-col text-center">
+	<div class="text-gray-200">{title}</div>
+	<div class="text-gray-100 font-bold text-4xl">
+		{`${formatAsCurrency ? '$' : ''}${num.toLocaleString()}`}
+	</div>
+	<div class={changeTextColor}>
+		{`${formatAsCurrency ? '$' : ''}${change.toLocaleString()}`}
+	</div>
+</div>
---
+++ src/components/stat.ts
@@ -0,0 +1,4 @@
+export enum GoodOrBad {
+	GOOD,
+	BAD,
+}

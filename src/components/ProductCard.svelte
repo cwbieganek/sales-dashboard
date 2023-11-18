1f1a880c2eca1e9f51e24aa884b761484ae2fb3c
Chris Bieganek <cwbieganek@gmail.com>
2023-11-18 13:06:03 -0600

    Update the ProductCard styling so it works with dark mode

--- src/components/ProductCard.svelte
+++ src/components/ProductCard.svelte
@@ -4,23 +4,23 @@
 	export let product: Product;
 </script>
 
-<div class="max-w-sm rounded overflow-hidden shadow-lg">
+<div class="max-w-sm rounded overflow-hidden bg-gray-900 border border-gray-800">
 	<img class="w-full" src={product.thumbnail} alt={product.description} />
 	<div class="px-6 py-4">
-		<div class="font-bold text-xl mb-2">{product.name}</div>
-		<div class="text-gray-700 text-base">
+		<div class="font-bold text-xl mb-2 text-white">{product.name}</div>
+		<div class="text-gray-100 text-base">
 			<p>{product.description}</p>
 			<div class="grid grid-cols-2 gap-2">
-				<div>Category:</div>
-				<div>{product.category}</div>
-				<div>List Price:</div>
+				<div class="font-bold">Category:</div>
+				<div class="font-semibold">{product.category}</div>
+				<div class="font-bold">List Price:</div>
 				<div class="font-semibold">${product.listPrice}</div>
-				<div>Wholesale Cost:</div>
+				<div class="font-bold whitespace-nowrap">Wholesale Cost:</div>
 				<div class="font-semibold">${product.wholesaleCost}</div>
-				<div>Profit:</div>
+				<div class="font-bold">Profit:</div>
 				<div class="font-semibold">${product.listPrice - product.wholesaleCost}</div>
-				<div>Sales:</div>
-				<div>{product.sales.toLocaleString()}</div>
+				<div class="font-bold">Sales:</div>
+				<div class="font-semibold">{product.sales.toLocaleString()}</div>
 			</div>
 		</div>
 	</div>

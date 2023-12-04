6e0fcabd016cb5f9c2357b178b5a9655e027b9bd
Chris <cwbieganek@gmail.com>
2023-12-04 09:20:29 -0600

    Show a 404 page if a product cannot be found (#29)
    * Rename the .find callback's parameter to productJson to avoid naming conflict.
    
    * Show a 404 page if a product is not found

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	const error = $page.error!; // error WILL be defined, despite what ts says
+</script>
+
+<h1 class="text-white text-center text-2xl mt-8">{error.message}</h1>
--- src/routes/products/[id]/+page.server.ts
+++ src/routes/products/[id]/+page.server.ts
@@ -1,9 +1,19 @@
+import { error } from '@sveltejs/kit';
 import type { Product } from '@/components/Product';
 import { productsSchema } from '../productsSchema';
 import rawProducts from '../products.json';
 
 export const load = ({ params }) => {
 	const productsFromJson = productsSchema.parse(rawProducts) as Product[];
+	const product = productsFromJson.find((productJson) => {
+		return productJson.id === parseInt(params.id);
+	});
+
+	if (!product) {
+		throw error(404, {
+			message: `Product #${params.id} not found!`,
+		});
+	}
 
 	return {
 		product: productsFromJson.find((product) => {
